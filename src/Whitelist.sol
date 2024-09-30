c1b1f679cc4c4842d059d55d19f2391be8bcb928
Vector <vector02web@gmail.com>
2024-10-01 00:00:27 +0100

    completed contract

--- src/Whitelist.sol
+++ src/Whitelist.sol
@@ -2,33 +2,49 @@
 
 pragma solidity ^0.8.18;
 
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 contract Whitelist {
-    uint256 private totalSupply = 1 * 10 ** 9;
-    uint256 private allocatedTokens = 100;
+    string private tokenName = "Ugly Token";
+    string private tokenTicker = "UGT";
+    uint256 private totalSupply = 1_000_000_000; // total supply is 1B
+    uint256 private allocatedTokens = 100 * 10 ** 18;
     address[] private whitelistedAddresses;
 
+    event Transfers(address indexed from, address indexed to, uint256 amount);
     address public i_owner;
+    mapping(address => uint256) public balanceOf;
 
     constructor() {
         i_owner = msg.sender;
+        balanceOf[i_owner] = totalSupply;
     }
 
     function registerForEligibility() public {
         whitelistedAddresses.push(msg.sender);
     }
 
-    function distributingTokens() public {
-        for (uint256 i = 0; i < whitelistedAddresses.length; i++) {}
+    function transferTokens(
+        address recipient,
+        uint256 amount
+    ) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient Balance!!");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfers(msg.sender, recipient, amount);
+        return true;
     }
 
-    function claimTokens() public view returns (bool) {
+    function distributingTokens() public onlyOwner {
+        require(
+            balanceOf[i_owner] >= allocatedTokens,
+            "Not enough tokens to allocate."
+        );
         for (uint256 i = 0; i < whitelistedAddresses.length; i++) {
-            require(
-                whitelistedAddresses[i] == msg.sender,
-                "You're not Eligible"
-            );
+            transferTokens(whitelistedAddresses[i], allocatedTokens);
         }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == i_owner, "This is not the contract owner.");
+        _;
     }
 }
